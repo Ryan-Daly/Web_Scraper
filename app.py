bf630e75c7b88a71d531923a84a0150236123881
Ryan Daly <ryan@macusers-iMac.local>
2019-07-31 14:57:25 +0100

    seperate url function for website parsed

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
 import requests
 import time
+import re
 
 from pages.all_products_page import AllProductsPage
 
+url = 'https://www.wilko.com/en-uk/pets/cats-kittens/cat-food-treats/c/1077?page=0&pageSize=24'
 
-page_content = requests.get('https://www.wilko.com/en-uk/pets/cats-kittens/cat-food-treats/c/1077?page=0&pageSize=24').content
-
-
-page = AllProductsPage(page_content)
+#page_content = requests.get(url).content
 
 
-print(page.products)
+#page = AllProductsPage(page_content)
+
+#products = page.products
+
+def website_parsed(url):
+    pattern = '(https://www.[a-z]+.com/)'
+    matcher = re.search(pattern, url)
+    website = matcher.group(1)
+    return f'{website}'
+
+website = website_parsed(url)
+
+#print(products)
+print(website)
